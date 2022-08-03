19be1e99c16307886d5d49026ea2d91f88a2438e
k0smo <sawallace7@gmail.com>
2022-08-03 10:25:27 -0700

    week6

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,12 +17,32 @@
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
+  { name: "arthur dent", occupation: "a radio employee" },]
+
+describe("capitalize", () => {
+  it("capitalize", () => {
+    expect(capitalize(people)).toEqual(["Ford Prefect is a hitchhiker", "Zaphod Beeblebrox is president the galaxy", "Arthur Dent is a radio employee"])
+  })
+}
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 
 // b) Create the function that makes the test pass.
+// Psuedo code:
+// 1. Create a function that takes in an array of people objects.
+// 2. Create a new array.
+// 3. Loop through the length array of people.
+// 4. Create a new string.
+// 5. Add the name and occupation to the string.
+// 6. Add the string to the new array.
+// 7. Return the new array.
+
+  const capitalize = (people) => {
+    let newArray = [];
+    for (let i = 0; i < people.length; i++) {
+      let newString = `${people[i].name} is ${people[i].occupation}`;
+      newArray.push(newString);
+  }
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -33,10 +53,31 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
-
+describe("remainders", () => {
+  it("should return an array of numbers", () => {
+    expect(remainders(hodgepodge1)).toEqual([2, 0, -1, 0]);
+    expect(remainders(hodgepodge2)).toEqual([2, 1, -1]);
+})
+})
 
 // b) Create the function that makes the test pass.
 
+// Psuedo code:
+// 1. Create a function that takes in a mixed data array.
+// 2. return hodgepodge.map(item => { 
+// 3. if (typeof item === "number") {
+// 4. return item % 3;
+// 5. Else return item 0
+
+  const remainders = (hodgepodge) => {
+    return hodgepodge.map(item => {
+      if (typeof item === "number") {
+        return item % 3
+      } else {
+        return 0
+      }
+  })
+  }
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -47,5 +88,27 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+  describe("cubeAndSum", () => {
+    it("should return a number", () => {
+      expect(cubeAndSum(cubeAndSum1)).toEqual(99);
+      expect(cubeAndSum(cubeAndSum2)).toEqual(1125);
+    })
+
+  })
+
 
 // b) Create the function that makes the test pass.
+// psuedocode:
+// 1. Create a function that takes in an array of numbers.
+// 2. Create a variable that is equal to 0.
+// 3. Loop through the array.
+// 4. Add the cube of the number to the variable.
+// 5. Return the variable, the sum of the cubes of the numbers.
+  
+const cubeAndSum = (numbers) => {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += Math.pow(numbers[i], 3);
+    return sum;  
+  }
+}
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,46 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+# pseudocode:
+#  1. create a new blog post
+#  2. create a new comment for the blog post
+
 class BlogPostsController < ApplicationController
   def index
     # ---2)
+    # pseudocode:
+    #   - get all blog posts
+    #   - get all comments
     @posts = BlogPost.all
   end
 
   # ---3)
+  # pseudocode:
+  #   - get a blog post
+  #   - get all comments for that blog post
+  
   def show
     @post = BlogPost.find(params[:id])
   end
 
   # ---4)
+  # pseudocode:
+  #   - get a new blog post
+  #   - get comments for that blog post
+
   def new
     @post = BlogPost.new
   end
 
   def create
     # ---5)
+    # pseudocode:
+    # get a new blog post
+    # post is valid?
+    #   - yes: redirect to blog_post_path
+    #   - no: redirect to new_blog_post_path
+    # get all comments for that blog post
+
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -35,12 +57,23 @@ class BlogPostsController < ApplicationController
 
   def edit
     # ---6)
+    # pseudocode:
+    # find a blog post
+    # find comments for that blog post
+
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
     # ---7)
+    # pseudocode:
+    # update a blog post
+    # update comments for that blog post
+    # if update is successful, redirect to blog_post_path
+    # if update is not successful, redirect to edit_blog_post_path
+
+
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -55,14 +88,27 @@ class BlogPostsController < ApplicationController
       redirect_to blog_posts_path
     else
       # ---8)
+      # pseudocode:
+      # if destroy is not successful, redirect to blog_post_path
+
       redirect_to blog_post_path(@post)
     end
   end
 
   # ---9)
+  # pseudocode:
+  #   - get a blog post
+  #   - get all comments for that blog post params
+
   private
   def blog_post_params
     # ---10)
+    # pseudocode:
+    #   - get params
+    #   - permit blog post attributes
+    #   - permit comment attributes
+    #  - return params
+    
     params.require(:blog_post).permit(:title, :content)
   end
 end
